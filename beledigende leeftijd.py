14b1f9fa60c6f3e932c2275557268309803c1d5b
trojanili <133130158+trojanili@users.noreply.github.com>
2024-10-13 21:54:38 +0200

    Delete beledigende leeftijd.py

--- beledigende leeftijd.py
+++
@@ -1,8 +0,0 @@
-def xsom():
-    get1 = (int(input('welke getal wil je x doen')))
-    get2 = (int(input('welke getal wil je keer dat doen')))
-    antwoord = get1 * get2
-    return antwoord
-
-uitkomst = xsom()
-print (uitkomst)
