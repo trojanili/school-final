5c8228dbf9558b6bb4aeebd758297cd629fe433a
ilias <iliasw8@Hotmail.com>
2024-10-13 21:46:53 +0200

    dit is een git test

--- nano project/eerste nano spel - nummer raden.py
+++ nano project/eerste nano spel - nummer raden.py
@@ -7,7 +7,13 @@ pogingen = 8
 
 def gokken():
     for poging in range(pogingen):
-        gok = int(input(f'poging {poging + 1}: raad het nummer: '))
+        while True:
+            try:
+                gok = int(input(f'poging {poging + 1}: raad het nummer: '))
+                break
+            except ValueError:
+                print("Ongeldige invoer. Voer alstublieft een getal in.")
+
         if gok == getal:
             print('Wauw!!, Goed gedaan, je hebt het geraden!')
             break
@@ -15,14 +21,18 @@ def gokken():
             print('Het getal is hoger.')
         elif gok > getal:
             print('Het getal is lager.')
+
     print('helaas, volgende keer beter!')
 
 
 while True:
-    print('Denk jij dat je het nummer kan raden binnen', pogingen, 'pogingen? kom maar op!')
-    print('je moet een getal raden tussen 1 tot en met', maxGetal)
+    print('Denk jij dat je het nummer kan raden binnen', pogingen, 'pogingen? Kom maar op!')
+    print('Je moet een getal raden tussen 1 tot en met', maxGetal)
     print('psstt, ik zal je een beetje helpen')
     gokken()
 
-# durf je het nog keer te proberen 'ja' of 'nee'
-# menu met moeilijkheidsgraad
+    opnieuw_spelen = input("Durf je het nog een keer te proberen? (ja of nee): ").lower()
+    if opnieuw_spelen == 'nee':
+        print("tot de volgende keer!")
+        break
+
